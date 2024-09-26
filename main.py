5bca4baa2e75987d61bf1755069321a3cec49e09
aryantiwari4451 <at108@snu.edu.in>
2024-09-26 20:32:54 +0530

    importing librarires

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 # IR LAB 2 Asignment
 
+import os
+import re
+from collections import defaultdict
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
