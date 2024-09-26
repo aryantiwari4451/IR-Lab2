500302e03d567973fbd0450a7687d2627f6418b7
aryantiwari4451 <at108@snu.edu.in>
2024-09-26 23:36:41 +0530

    search and ranking filter

--- main.py
+++ main.py
@@ -50,9 +50,25 @@ def calculate_tf_idf_weights(dictionary, doc_lengths, N):
             tf_idf_weights[term][doc_id] = (1 + math.log10(tf)) * idf
     return tf_idf_weights
 
-
-
-
+# Function to search and rank documents
+def search_and_rank(tf_idf_weights, doc_lengths, doc_filenames, query):
+    query_tokens = preprocess_text(query)
+    query_vector = {}
+    for token in query_tokens:
+        if token in tf_idf_weights:
+            query_vector[token] = (1 + math.log10(query_tokens.count(token))) * math.log10(len(doc_lengths) / len(tf_idf_weights[token]))
+    doc_scores = {}
+    for doc_id in doc_lengths:
+        doc_vector = {}
+        for token, postings in tf_idf_weights.items():
+            if doc_id in postings:
+                doc_vector[token] = postings[doc_id]
+        dot_product = sum(query_vector[token] * doc_vector[token] for token in query_vector if token in doc_vector)
+        magnitude_query = math.sqrt(sum(query_vector[token] ** 2 for token in query_vector))
+        magnitude_doc = math.sqrt(sum(doc_vector[token] ** 2 for token in doc_vector))
+        cosine_similarity = dot_product / (magnitude_query * magnitude_doc)
+        doc_scores[doc_id] = cosine_similarity
+    return [(doc_filenames[doc_id], score) for doc_id, score in sorted(doc_scores.items(), key=lambda x: (-x[1], x[0]))[:10]]
 
 
 
